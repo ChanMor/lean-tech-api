7837ee3ee1fa50a14fa1a18d57ce1bd2d3c6b725
Franz Christian Morelos <morelos.franzchristian@gmail.com>
2025-01-11 17:33:29 +0800

    Fixed json

--- api/main.py
+++ api/main.py
@@ -74,7 +74,7 @@ async def retrieve_cases(name: str):
 
     try:
         response = requests.post(url, json=payload, headers=headers)
-        return {"status": "success", "cases": response.text}
+        return {"status": "success", "cases": response.json()}
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=400, detail="Error")
 
